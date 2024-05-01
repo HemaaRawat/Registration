29c00a4a6915614e9e77f03465641fdc3e8910a4
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2024-05-01 16:45:16 +0530

    fix server configs

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ require("./db/conn");
 //cors error
 app.use(
   cors({
-    origin: "http://localhost:5000",
+    origin: "http://localhost:8000",
   })
 );
 
--- server/db/conn.js
+++ server/db/conn.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 
-//Database connection
-// mongoose
+// Database connection
 
 const DB = process.env.DATABASE; //to make password secure
 mongoose.connect(DB, {
-    useNewUrlParser: true,
-    // useCreateIndex: true,
-    useUnifiedTopology: true,
-    // useFindAndModify: false,
-  });
-  
-  const connection = mongoose.connection;
-  connection.on("error", (err) => console.log(err));
-  connection.on("connected", () => console.log("Mongoose Connection Succesful"));
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  useUnifiedTopology: true,
+  // useFindAndModify: false,
+});
+
+const connection = mongoose.connection;
+connection.on("error", (err) => console.log(err));
+connection.on("connected", () => console.log("Mongoose Connection Succesful"));
